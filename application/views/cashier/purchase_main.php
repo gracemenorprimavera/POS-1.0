3f4bc28d37650e782f2e3fa8f52d189ccd431b3d
Nicole I. Tibay <neequole@gmail.com>
2013-06-10 23:01:50 +0800

    barcode autofocus
    this is an html5 attribute, just tell me if ok na to or need din na hind
    pang html5? eto ung after ng enter mgffocus sa barcode input box ung
    page.

--- application/views/cashier/purchase_main.php
+++ application/views/cashier/purchase_main.php
@@ -2,7 +2,7 @@
 
 <?php echo $message; ?>
 <?php echo form_open('cashier/add_item') ?>	
-	<label> Bar Code </label> <input type="search" name="search_item" id="search_item" class="tags">
+	<label> Bar Code </label> <input type="search" name="search_item" id="search_item" class="tags" autofocus />
 	<label> Quantity </label> <input type="number" name="quantity" value="1" min="1" >
 	<?php echo form_submit(array('class'=>'button', 'name'=>'enterItem_submit'),'Enter'); ?>
 <?php echo form_close(); ?>
