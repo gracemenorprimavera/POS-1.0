28a6259a1e8d3007ce4fdeab62968cc87cfc7ecc
willgrace <graycelsantos@gmail.com>
2013-06-22 14:04:43 +0800

    Sales new interface
    Updated sales_form and new css

--- application/views/forms/sales_form.php
+++ application/views/forms/sales_form.php
@@ -1,27 +1,23 @@
 
-<div class="error"><?php echo $message; ?></div>
+<div class="error" style="font-size: 18px;height: 19px;padding-bottom: 5px;"><b><?php echo $message; ?></b></div>
 <?php echo form_open('sales/add_item'); 
 $this->load->helper('string');
 ?>
-<div id="inside">
-<div class="info">[Date: XXXXX]	[Time: XXXXX] <br>
-[Transaction No.: XXXXX]</div>
-<div class="who">[Customer: XXXXX]</div>
-</div>
 
+<div id="cashier">
+<div id="sales">
 	<input type='hidden' class='hItemPurchase' name='hItemPurchase' />
-	<label> Item </label><select name='searchMode'><option value="Barcode">Barcode</option><option value="Itemcode">Item code</option></select>
+	<label> Item </label>
+	<select name='searchMode'><option value="Barcode">Barcode</option><option value="Itemcode">Item code</option></select>
 	<input type="text" name="search_item" id="search_item" class="tags" tabindex="1" autofocus />
 	<label> Quantity </label> <input type="number" name="quantity" value="1" min="1" tabindex="2">
 	<input class="button" type="submit" name="submit" value="Submit" />
 <?php echo form_close(); ?>
 
-<div id="purchase_list">
 
 
 <?php echo form_open('sales/do_purchase') ?>
-<div id="sales">
-<table cellpadding="6" cellspacing="1" style="width:100%;" border="1px solid black">
+<table cellpadding="6" cellspacing="1" style="width:100%;">
 <thead >
 <tr>
   <th>QTY</th>
@@ -31,7 +27,10 @@ $this->load->helper('string');
   <th>Action</th>
 </tr>
 </thead>
-<tbody>
+</table>
+<div id="list">
+<table cellpadding="6" cellspacing="1" style="width:100%;">
+<tbody >
 <?php $i = 1; ?>
 
 <?php foreach ($this->cart->contents() as $items): ?>
@@ -74,13 +73,24 @@ $this->load->helper('string');
 </tbody>
 </table>
 </div>
-
-<div class="label">Amount Due:
-	<div id="total">P<?php echo $this->cart->format_number($this->cart->total()); ?></div>
 </div>
+
+
+<div id="purchase">
+
+<div id="inside" >
+<div class="info">[Date: XXXXX]	[Time: XXXXX] <br>
+[Transaction No.: XXXXX]</div>
+<div class="who">[Customer: XXXXX]</div>
+</div>
+<br>
+<div id="currentItem">[insert current ITEM PRICE QTY? here]</div>
+<div class="label" >Amount Due:
+	<div id="total" >P<?php echo $this->cart->format_number($this->cart->total()); ?></div>
+</div><br>
 <div id="tc">[Tendered: XXXXX]	<br> [Change: XXXXX] </div>
 
-
+<div id="cart" >
 <?php
 	if($this->cart->total_items() > 0) {
 		
@@ -128,10 +138,12 @@ echo form_close();
 
 //echo form_open('cashier/cancel_trans');
  	
-	echo anchor('sales/cancel_trans',' Cancel Transaction','class="error"',array('onclick' => "return confirm ('Are you sure want to cancel this transaction?')")); 
+	echo '<b>'.anchor('sales/cancel_trans',' Cancel Transaction','class="error"',array('onclick' => "return confirm ('Are you sure want to cancel this transaction?')")).'</b>'; 
 	
 //echo form_close();
  ?>	
+</div>
+</div>
 </div>
 
 
